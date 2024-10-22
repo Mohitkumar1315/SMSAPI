82fc1099d9d1e1e6174ab33dc120fcd0d50291ed
Mohitkumar1315 <mvkrampur1315@gmail.com>
2024-10-22 13:11:41 +0530

    Code for consume SMS api

--- SMSAPi/src/main/java/com/subhsms/Controllers/SubhSMSController.java
+++ SMSAPi/src/main/java/com/subhsms/Controllers/SubhSMSController.java
@@ -1,5 +1,30 @@
 package com.subhsms.Controllers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-public class SubhSMSController {
+import com.subhsms.services.OtpService;
 
+@RestController
+@RequestMapping("/api/otp")
+public class SubhSMSController 
+{
+	@Autowired 
+	OtpService otpservice;
+	
+	@GetMapping("/test")
+	public String test()
+	{
+		return "method testing in SubhSMSController";
+	}
+	
+	@PostMapping("/send/{contactnumber}")
+	public String sendOtp(@PathVariable String contactnumber) {
+	    System.out.println("You are in sendOTP method");
+	    String msg = "OTP for test login {1235}";
+	    return otpservice.sendOtp(contactnumber, msg);
+	}
 }
--- SMSAPi/src/main/java/com/subhsms/services/OtpService.java
+++ SMSAPi/src/main/java/com/subhsms/services/OtpService.java
@@ -1,5 +1,4 @@
 package com.subhsms.services;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -10,23 +9,42 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class OtpService {
 
-    private static final String OTP_API_URL = "https://automate.nexgplatforms.com/api/v1/sms/save";
+    private static final String OTP_API_URL = "https://automate.nexgplatforms.com/api/v1/sms";
 
-    @Autowired
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
+
+    public OtpService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public String sendOtp(String contactNumber, String message) {
-        HttpHeaders headers =  new HttpHeaders();
+        HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "09de330ad643416380ecadbbff117ebe");
-        headers.set("Content-Type", "application/json");
+        
+        // Escape curly braces in the message
+        String formattedMessage = message.replace("{", "%7B").replace("}", "%7D");
 
-        // Create request body
-        String requestBody = String.format("{\"header\":\"SUBHFN\",\"contactnumber\":\"%s\",\"message\":\"%s\",\"messageType\":\"normal\",\"messageid\":\"3fbdd0b1-030f-4359-b21c-4334y4ffe\",\"serviceType\":\"transactional\",\"entityid\":\"1201159195854796804\",\"templateid\":\"1207161596312561180\"}", 
-                                            contactNumber, message);
+        // URL with query parameters
+        String url = String.format(
+            "%s?contactnumber=%s&header=SUBHFN&message=%s&messageType=normal&messageid=3fbdd0b1-030f-4359-b21c-4334y4ffe&serviceType=transactional&entityid=1201159195854796804&templateid=1207161596312561180",
+            OTP_API_URL, contactNumber, formattedMessage);
 
-        HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
-        ResponseEntity<String> responseEntity = restTemplate.exchange(OTP_API_URL, HttpMethod.POST, requestEntity, String.class);
+        HttpEntity<String> requestEntity = new HttpEntity<>(headers);
 
-        return responseEntity.getBody();
+        try {
+            // Make the API request (GET method)
+            ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
+
+            // Log the status and response body
+            System.out.println("Response Status: " + responseEntity.getStatusCode());
+            System.out.println("Response Body: " + responseEntity.getBody());
+
+            return responseEntity.getBody();
+        } catch (Exception e) {
+            // Log and rethrow the exception for debugging purposes
+            System.err.println("Error occurred: " + e.getMessage());
+            throw e;
+        }
     }
+
 }
