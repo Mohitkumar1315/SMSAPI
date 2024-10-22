882e10b5b247200133de2ef840fb54fbebf3fe75
Mohitkumar1315 <mvkrampur1315@gmail.com>
2024-10-22 14:52:49 +0530

    SMS api test controller

--- SMSAPi/src/main/java/com/subhsms/Controllers/SubhSMSController.java
+++ SMSAPi/src/main/java/com/subhsms/Controllers/SubhSMSController.java
@@ -1,30 +1,36 @@
 package com.subhsms.Controllers;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.subhsms.services.OtpService;
-
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
 @RestController
 @RequestMapping("/api/otp")
 public class SubhSMSController 
 {
-	@Autowired 
-	OtpService otpservice;
-	
-	@GetMapping("/test")
-	public String test()
-	{
-		return "method testing in SubhSMSController";
-	}
-	
-	@PostMapping("/send/{contactnumber}")
-	public String sendOtp(@PathVariable String contactnumber) {
-	    System.out.println("You are in sendOTP method");
-	    String msg = "OTP for test login {1235}";
-	    return otpservice.sendOtp(contactnumber, msg);
-	}
+	@GetMapping("/send/{contactnumber}")
+    public String sendOtpViaUnirest(@PathVariable String contactnumber) {
+        try {
+            // Set timeouts and make the Unirest request
+           // Unirest.setTimeouts(0, 0);
+            HttpResponse<String> response = Unirest.get("https://automate.nexgplatforms.com/api/v1/sms"
+                    + "?contactnumber=" + contactnumber
+                    + "&header=SUBHFN"
+                    + "&message=OTP%20for%20login%3A%7B1253%7D"
+                    + "&messageType=normal"
+                    + "&messageid=3fbdd0b1-030f-4359-b21c-4334y4ffe"
+                    + "&serviceType=transactional"
+                    + "&entityid=1201159195854796804"
+                    + "&templateid=1207161596312561180")
+                    .header("Authorization", "09de330ad643416380ecadbbff117ebe")
+                    .asString();
+
+            // Return the response body
+            return response.getBody();
+        } catch (Exception e) {
+            // Handle any exceptions and return an error message
+            return "Error occurred: " + e.getMessage();
+        }
+    }
 }
