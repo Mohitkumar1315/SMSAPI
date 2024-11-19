da8321ca3cb0f47dc0f73f80223dabb704c769ff
Mohitkumar1315 <mvkrampur1315@gmail.com>
2024-11-19 12:13:21 +0530

    Pushing nextDemo project into the SubhlakshmiWeb

--- NextDemo/src/main/java/com/subh/NextDemoApplication.java
+++ NextDemo/src/main/java/com/subh/NextDemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NextDemoApplication {
+public class NextDemoApplication { ///application 
 
 	public static void main(String[] args) {
 		SpringApplication.run(NextDemoApplication.class, args);
